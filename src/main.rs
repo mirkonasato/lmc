13b070554a3ac303e8f1b39153e7fc814b1e1ad5
Mirko Nasato <mirko@nasato.dev>
2024-11-25 15:39:57 +0000

    Ignore empty lines in interactive mode

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod console;
 
 use std::io::{self, IsTerminal};
 
+use anyhow::bail;
+use config::Config;
 use futures_util::StreamExt;
 
 use crate::api::{ApiClient, ApiError, Message, Role};
@@ -16,48 +18,19 @@ async fn main() -> anyhow::Result<()> {
         println!("{} v{}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }
-
     let config = config::get_config(&args)?;
-    let api_client = ApiClient::new(&config);
-    let interactive = io::stdin().is_terminal();
-
-    let mut console = Console::new()?;
-    let mut messages: Vec<Message> = Vec::new();
-    if let Some(system_prompt) = config.system_prompt {
-        messages.push(Message::new(Role::System, &system_prompt));
-    }
-
-    if interactive {
-        println!(
-            "[i] Chatting with \"{}\" at \"{}\"",
-            config.model, config.api_url
-        );
-        loop {
-            match console.read_interactive_input()? {
-                None => break, // EOF
-                Some(command) if command == "/q" || command == "/quit" => break,
-                Some(command) if command == "/r" || command == "/retry" => {
-                    if let Some(message) = messages.last() {
-                        if message.role == Role::Assistant {
-                            messages.pop();
-                        }
-                    }
-                }
-                Some(user_prompt) => messages.push(Message::new(Role::User, &user_prompt)),
-            }
-            let result = get_and_print_completion(&api_client, &messages, config.stream).await;
-            match result {
-                Ok(completion) => messages.push(Message::new(Role::Assistant, &completion)),
-                Err(error) => eprintln!("[e] {:?}", error),
-            }
-        }
+    if io::stdin().is_terminal() {
+        run_interactive_loop(config).await
     } else {
-        let user_prompt = console.read_piped_input()?;
-        messages.push(Message::new(Role::User, &user_prompt));
-        get_and_print_completion(&api_client, &messages, config.stream).await?;
+        run_with_piped_input(config).await
     }
+}
 
-    Ok(())
+fn create_messages(system_prompt: Option<String>) -> Vec<Message> {
+    match system_prompt {
+        None => Vec::new(),
+        Some(prompt) => vec![Message::new(Role::System, &prompt)],
+    }
 }
 
 async fn get_and_print_completion(
@@ -81,4 +54,52 @@ async fn get_and_print_completion(
         println!("{}", completion);
         Ok(completion)
     }
+}
+
+async fn run_interactive_loop(config: Config) -> anyhow::Result<()> {
+    let api_client = ApiClient::new(&config);
+    let mut console = Console::new()?;
+    let mut messages = create_messages(config.system_prompt);
+    println!(
+        "[i] Chatting with \"{}\" at \"{}\"",
+        config.model, config.api_url
+    );
+    loop {
+        match console.read_interactive_input()? {
+            None => break, // EOF
+            Some(command) if command == "/q" || command == "/quit" => break,
+            Some(command) if command == "/r" || command == "/retry" => {
+                if let Some(message) = messages.last() {
+                    if message.role == Role::Assistant {
+                        messages.pop();
+                    }
+                }
+            }
+            Some(user_prompt) => {
+                if user_prompt.is_empty() {
+                    continue; // ignore empty lines
+                }
+                messages.push(Message::new(Role::User, &user_prompt));
+            }
+        }
+        let result = get_and_print_completion(&api_client, &messages, config.stream).await;
+        match result {
+            Ok(completion) => messages.push(Message::new(Role::Assistant, &completion)),
+            Err(error) => eprintln!("[e] {:?}", error),
+        }
+    }
+    Ok(())
+}
+
+async fn run_with_piped_input(config: Config) -> anyhow::Result<()> {
+    let api_client = ApiClient::new(&config);
+    let mut console = Console::new()?;
+    let mut messages = create_messages(config.system_prompt);
+    let user_prompt = console.read_piped_input()?;
+    if user_prompt.is_empty() {
+        bail!("Expected a prompt to be supplied via stdin but it was empty");
+    }
+    messages.push(Message::new(Role::User, &user_prompt));
+    get_and_print_completion(&api_client, &messages, config.stream).await?;
+    Ok(())
 }
